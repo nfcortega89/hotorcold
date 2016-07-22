767a919dab4fe92c61419cb11184794feae3419b
Nikko Ortega <NFCOrtega89@gmail.com>
2016-07-22 08:14:39 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    $(".new").click(function() {
+        var num = generate();
+        console.log(num);
+
+        var counter = 1;
+
+        $(".button").on("click", function(event) {
+            event.preventDefault();
+            $('#count').text(counter);
+            counter++;
+            var input = parseInt($('#userGuess').val());
+            var difference = Math.abs(num - input);
+            $('ul#guessList').append('<li>' + parseInt($('#userGuess').val()) + '</li>').show();
+            console.log(input);
+            if (difference === 0) {
+                $('h2').text("You got it!");
+            } else if (difference < 10) {
+                $('h2').text("Very hot!");
+            } else if (difference < 20) {
+                $('h2').text("Getting warm!");
+            } else if (difference < 30) {
+                $('h2').text("Warm!");
+            } else if (difference < 40) {
+                $('h2').text("You're cold!");
+            } else if (difference < 50) {
+                $('h2').text("Brrrrr...it's cold in here!");
+            } else if(input > 100 || input < 0){
+                $('h2').text("Invalid!")
+            }
+        })
+    });
+    var generate = function() {
+        return Math.floor(Math.random() * 100);
+    };
+
+    /*--- Display information modal box ---*/
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
+
+})
